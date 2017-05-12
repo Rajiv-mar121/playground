877451796dcc6abd5031e7e2506cacf8024081ce
Rajiv <rajiv.kumar.102@gmail.com>
2017-05-13 02:43:23 +0530

    Hello World Java Config

---
+++ Spring4MVCHelloWorldNoXMLDemo/src/com/websystique/springmvc/controller/HomeControllerTest1.java
@@ -0,0 +1,24 @@
+package com.websystique.springmvc.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+
+//import spittr.web.HomeController;
+public class HomeControllerTest1 {
+	@Test
+	public void testHelloPage() throws Exception {
+		HelloWorldController controller = new HelloWorldController();
+		MockMvc mockMvc = standaloneSetup(controller).build();
+		mockMvc.perform(get("/")).andExpect(view().name("welcome"));
+	}
+	
+	@Test
+	public void testHelloAgain() throws Exception {
+		HelloWorldController controller = new HelloWorldController();
+		MockMvc mockMvc = standaloneSetup(controller).build();
+		mockMvc.perform(get("/helloagain")).andExpect(view().name("welcome"));
+	}
+}
