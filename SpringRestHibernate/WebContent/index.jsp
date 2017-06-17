6477eb9f785f1ed900b87d2c753a1bf0f97d4fa6
Rajiv-mar121 <rajiv.kumar.102@gmail.com>
2017-06-18 03:19:25 +0530

    #Rest Spring

---
+++ SpringRestHibernate/WebContent/index.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Hello Jsp</title>
+</head>
+<body>
+<h1>SpringRest</h1>
+<form action="./call">  <!-- hello.ds -->
+
+NAME:<input type="text" name="name"/>
+	<input type="submit" value="sayHello"/>
+
+</form>
+</body>
+</html>
---
+++ SpringRestHibernate/src/main/java/com/spring/rest/controller/MyAppController.java
@@ -0,0 +1,29 @@
+package com.spring.rest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+//@RequestMapping("/call")   ///hello.ds
+public class MyAppController {
+
+	@RequestMapping(value="/call" , method = RequestMethod.GET)
+    public String sayHello(@RequestParam("name") String name, @RequestParam(value="name",
+    		defaultValue="No Value") String name1,ModelMap model) throws Exception{
+    	
+    	System.out.println("Raj---> home rest page "+name+" --->"+name1);
+    //	this.dbCheck();
+        model.addAttribute("greeting"," " +name+" Hello World from Spring 4 MVC");
+        return "success";
+    }
+	
+	//http://www.journaldev.com/3358/spring-requestmapping-requestparam-pathvariable-example
+	@RequestMapping(value="/login" , method = RequestMethod.GET)
+	public void login(@RequestParam("loginName") String loginName)
+	{
+		System.out.println("Raj--->Login home rest page ..");
+	}
+}
