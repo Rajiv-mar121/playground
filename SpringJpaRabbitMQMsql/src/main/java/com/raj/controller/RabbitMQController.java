faa54cae5b427e2a6c36e641fea4e921f37822cf
Rajiv-mar121 <rajiv.kumar.102@gmail.com>
2018-07-26 01:39:41 +0530

    # Post mapping added new method

--- SpringJpaRabbitMQMsql/src/main/java/com/raj/controller/RabbitMQController.java
+++ SpringJpaRabbitMQMsql/src/main/java/com/raj/controller/RabbitMQController.java
@@ -1,13 +1,20 @@
 package com.raj.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.raj.domain.Employee;
 import com.raj.service.RabbitMQService;
 
 /**
@@ -42,4 +49,20 @@ public class RabbitMQController {
 
 		return "Message sent to the RabbitMQ JavaInUse Successfully";
 	}
+	
+	//,produces=MediaType.APPLICATION_JSON_VALUE ,  ,consumes=MediaType.APPLICATION_JSON_VALUE ,@RequestBody Employee emp
+	@PostMapping(value="/post",produces=MediaType.APPLICATION_JSON_VALUE,consumes=MediaType.APPLICATION_JSON_VALUE)
+	public List<Employee> post(@RequestBody Employee emp)
+	{
+		System.out.println(" Inside RabbitMQ");
+	//	rabbitMQService.saveEmp();
+		Employee e1=new Employee();
+		e1.setEmpId("1002");
+		e1.setEmpName("Rocky");
+		System.out.println(" Emp Data "+emp.getEmpName());
+		List<Employee> li=new ArrayList<Employee>();
+		li.add(e1);
+		return li;
+	}
+
 }
